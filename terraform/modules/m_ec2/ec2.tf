e4969925b702fabd39b609d3bc88a22c4a888e09
deviljin112 <hubert.swic@gmail.com>
2021-01-19 13:49:51 +0000

    Bugfixes for Terraform

--- terraform/modules/m_ec2/ec2.tf
+++ terraform/modules/m_ec2/ec2.tf
@@ -1,13 +1,17 @@
 
 # create aws ec2 instance
 resource "aws_instance" "ec2_instance" {
-	ami = var.ami_id
-	subnet_id = var.subnet_id
-	instance_type = var.instance_type
-	key_name = var.aws_key_name
-	associate_public_ip_address = var.associate_pub_ip
-	vpc_security_group_ids = [var.security_group_id]
-	tags = {
-		Name = "${var.name_tag}"
-	}
+  ami                         = var.ami_id
+  subnet_id                   = var.subnet_id
+  instance_type               = var.instance_type
+  key_name                    = var.aws_key_name
+  associate_public_ip_address = var.associate_pub_ip
+  vpc_security_group_ids      = [var.security_group_id]
+  tags = {
+    Name = var.name_tag
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo netdata-claim.sh -token=7fviTnD7DC68hnpCA8ON8RvBZ4dkWOzr5WeV9j6Z7j9iV0YAAK0zpltwpWr6Ihil7pUaDjXFrGZAktECA19365Ukfded3tBNYOniX4jLLstdaMwVMw4KG4zt4Q9LBbPs7MSso28 -rooms=d1dd6ee9-2feb-4b4d-85b1-55808fceead0 -url=https://app.netdata.cloud"
+  EOF
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,36 @@
 # VARIABLES FOR MAIN
 variable "region" {
-    default = "eu-west-1"
+  default = "eu-west-1"
 }
 
 variable "ami_app" {
-    default = "ami-0651ff04b9b983c9f"
+  default = "ami-0489ce290f126c3e4"
 }
 
 variable "ami_db" {
-    default = "ami-03646b6976790491d"
+  default = "ami-03646b6976790491d"
 }
 
 variable "ami_jenkins" {
-    default = "ami-0651ff04b9b983c9f"
+  default = "ami-05d324167a55228fe"
 }
 
 variable "ami_ubuntu" {
-    default = "ami-0dc8d444ee2a42d8a"
+  default = "ami-0dc8d444ee2a42d8a"
 }
 
 variable "instance_type" {
-    default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "aws_key_name" {
-    default = "eng74_fp_aws_key"
+  default = "eng74_fp_aws_key"
 }
 
 variable "aws_key_path" {
-    default = "~/.ssh/eng74_fp_aws_key.pem"
+  default = "~/.ssh/eng74_fp_aws_key.pem"
 }
 
 variable "extra_user_ip" {
-    default = "84.69.102.61"
+  default = "84.69.102.61"
 }
