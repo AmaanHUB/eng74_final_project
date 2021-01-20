3ff74ceefd3cd35ff9341065c461a8b1523c57ab
ldaijiw <lwaltmann@spartaglobal>
2021-01-20 14:08:26 +0000

    load balancing and autoscaling

---
+++ terraform/modules/m_app_lb/variables.tf
@@ -0,0 +1,11 @@
+# VARIABLES FOR LOAD BALANCER AND AUTOSCALING GROUP
+
+variable "app_ami" {}
+
+variable "instance_type" {}
+
+variable "app_sg_id" {}
+
+variable "public_subnet_id" {}
+
+variable "vpc_id" {}
