837b3467cffe396851006f28bd890bea2844bc93
ldaijiw <lwaltmann@spartaglobal>
2021-01-26 13:53:59 +0000

    added loadbalancer dns output

--- terraform/modules/m_app_lb/as_group.tf
+++ terraform/modules/m_app_lb/as_group.tf
@@ -12,7 +12,7 @@ resource "aws_launch_configuration" "app_launch_conf" {
     sudo hostname autoscale_app
     sudo sed -i "16ihostname=autoscale_app" /etc/netdata/netdata.conf
     sudo service netdata restart
-    sudo netdata-claim.sh -token=7fviTnD7DC68hnpCA8ON8RvBZ4dkWOzr5WeV9j6Z7j9iV0YAAK0zpltwpWr6Ihil7pUaDjXFrGZAktECA19365Ukfded3tBNYOniX4jLLstdaMwVMw4KG4zt4Q9LBbPs7MSso28 -rooms=d1dd6ee9-2feb-4b4d-85b1-55808fceead0 -url=https://app.netdata.cloud -id=$(uuidgen)
+    sudo netdata-claim.sh -token=7fviTnD7DC68hnpCA8ON8RvBZ4dkWOzr5WeV9j6Z7j9iV0YAAK0zpltwpWr6Ihil7pUaDjXFrGZAktECA19365Ukfded3tBNYOniX4jLLstdaMwVMw4KG4zt4Q9LBbPs7MSso28 -rooms=d1dd6ee9-2feb-4b4d-85b1-55808fceead0 -url=https://app.netdata.cloud -id=$(uuidgen) 
   EOF
 }
 
@@ -44,7 +44,7 @@ resource "aws_autoscaling_policy" "as_group_policy_maxcpu" {
   autoscaling_group_name = aws_autoscaling_group.app_as_group.name
   policy_type = "TargetTrackingScaling"
 
-  scaling_adjustment = 1
+  # scaling_adjustment = 1
   
   target_tracking_configuration {
     predefined_metric_specification {
